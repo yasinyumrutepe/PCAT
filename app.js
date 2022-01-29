5d1309da202f77298b047915de65e7f375369ed8
Yasin <yyumrutepe19@posta.pau.edu.tr>
2022-01-29 17:14:11 +0300

    Static Files Uploaded

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
-const fn = function () {
-  console.log('Function working');
-};
+const express = require('express');
+const path= require('path')
+const app = express();
+
+
+const myLogger=(req,res,next)=>{
+  console.log("Middleware Log 1");
+  next();
+}
+const myLogger2=(req,res,next)=>{
+  console.log("Middleware Log 1");
+  next();
+}
+//MIDDLEWARES
+app.use(express.static('public'))
+
+
+app.get('/', (req, res) => {
+
+ res.sendFile(path.resolve(__dirname,'temp/index.html'))
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı`);
+});
