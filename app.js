cafbbedefe15ba82d08cc64b6a8efccbf74c8496
Yasin <yyumrutepe19@posta.pau.edu.tr>
2022-02-03 16:34:38 +0300

    mvc katmanı eklendı

--- app.js
+++ app.js
@@ -1,24 +1,43 @@
 const express = require('express');
-const path= require('path')
+const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
+const methodOverride = require('method-override');
+const ejs = require('ejs');
+const { AstPath } = require('prettier');
+const exp = require('constants');
+const photoController = require('./controllers/photoControllers');
+const pageController = require('./controllers/pageController');
+
 const app = express();
 
+//Connect DB
+mongoose.connect('mongodb://localhost/pcat-test-db');
 
-const myLogger=(req,res,next)=>{
-  console.log("Middleware Log 1");
-  next();
-}
-const myLogger2=(req,res,next)=>{
-  console.log("Middleware Log 1");
-  next();
-}
+//Template Engine
+
+app.set('view engine', 'ejs');
+
 //MIDDLEWARES
-app.use(express.static('public'))
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(fileUpload());
+app.use(
+  methodOverride('_method', {
+    methods: ['POST', 'GET'],
+  })
+);
 
+//Routes
+app.get('/', photoController.getAllPhotos);
+app.get('/photos/:id', photoController.getPhoto);
+app.post('/photos', photoController.createPhoto);
+app.put('/photos/:id', photoController.updatePhoto);
+app.delete('/photos/:id', photoController.deletePhoto);
 
-app.get('/', (req, res) => {
-
- res.sendFile(path.resolve(__dirname,'temp/index.html'))
-});
+app.get('/about', pageController.getAboutPage);
+app.get('/add', pageController.getAddPage);
+app.get('/photos/edit/:id', pageController.getEditPage);
 
 const port = 3000;
 app.listen(port, () => {
