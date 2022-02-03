6a5137fdaaf4e4256cb6a8f45803cd1dbf79aebd
Yasin <yyumrutepe19@posta.pau.edu.tr>
2022-02-03 17:45:15 +0300

    Ready For Heroku

--- app.js
+++ app.js
@@ -11,7 +11,11 @@ const pageController = require('./controllers/pageController');
 const app = express();
 
 //Connect DB
-mongoose.connect('mongodb://localhost/pcat-test-db');
+mongoose.connect('mongodb+srv://root:Yasinyt123-@cluster0.ffwug.mongodb.net/pcat-db?retryWrites=true&w=majority').then(()=>{
+  console.log('DB CONNECTED !!')
+}).catch((err)=>{
+  console.log(err)
+})
 
 //Template Engine
 
@@ -39,7 +43,7 @@ app.get('/about', pageController.getAboutPage);
 app.get('/add', pageController.getAddPage);
 app.get('/photos/edit/:id', pageController.getEditPage);
 
-const port = 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Sunucu ${port} portunda başlatıldı`);
 });
